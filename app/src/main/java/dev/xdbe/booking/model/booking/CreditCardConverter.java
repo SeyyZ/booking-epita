a447319a26f12023c5c5f4634d0b96f3c3bacbc5
theo.rogue <theo.rogue@epita.fr>
2025-03-20 16:16:10 +0100

    feat: credit card number masking

--- app/src/main/java/dev/xdbe/booking/model/booking/CreditCardConverter.java
+++ app/src/main/java/dev/xdbe/booking/model/booking/CreditCardConverter.java
@@ -7,6 +7,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,8 +24,13 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
     @Override
     public String convertToEntityAttribute(String dbData) {
         // Step 5: Mask credit card number
-        // Insert your code here to mask credit card number        
-        return dbData;
+        // Insert your code here to mask credit card number       
+        int length = dbData.length();
+        String first4 = dbData.substring(0, 4);
+        String last4 = dbData.substring(length - 4);
+        String masked = "*".repeat(length - 8);
+
+        return first4 + masked + last4;
    }
 
     
